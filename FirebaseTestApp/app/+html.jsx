456f117605605c9942120dd3a4dd5058271d7475
creynolds8 <cameron8.cr@gmail.com>
2025-03-16 12:59:43 -0700

    seperate fetch and html concerns, adjust how each operates

--- FirebaseTestApp/app/+html.jsx
+++ FirebaseTestApp/app/+html.jsx
@@ -1,6 +1,4 @@
 export default function Root({children}) {
-  
-
   // this defines meta tags for the whole site regardless of endpoint 
   // but is picked up by Feacbook scraper
   return (
@@ -8,10 +6,9 @@ export default function Root({children}) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:description" content="PLACEHOLDER"/>
-        <meta property="og:title" content="PLACEHOLDER"/>
-        <meta property="og:image" content="PLACEHOLDER"/>
-        <title>Placeholder</title>
+        <meta property="og:description" content="___PLACEHOLDER___"/>
+        <meta property="og:title" content="___PLACEHOLDER___"/>
+        <meta property="og:image" content="___PLACEHOLDER___"/>
       </head>
       <body>
         {children}
--- FirebaseTestApp/app/event/[name].jsx
+++ FirebaseTestApp/app/event/[name].jsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-import { Image, Text } from "react-native";
-import { Link, useLocalSearchParams } from "expo-router";
+import { Text } from "react-native";
+import { useLocalSearchParams } from "expo-router";
 import { ActivityIndicator } from "react-native";
-import { Helmet } from 'react-helmet';
 
 export default function Event() {
+  
+  const { name } = useLocalSearchParams();  
+  const [loading, setLoading] = useState(true);
   const [event, setEvent] = useState({
     event_id: null,
     image_link: '',
@@ -15,62 +17,64 @@ export default function Event() {
     description: '',
   });
 
-  const [loading, setLoading] = useState(true);
-  const { name } = useLocalSearchParams();
-  
-  const ogTags = {
-    description: '',
-    title: '',
-    image: '',
-  }
-
- fetch(`http://localhost:3000/event/${name}`)
- .then(res => {
-      setLoading(false)
-      console.log(res);
-      return res;
-      })
-      .then(data => { 
-        // setEvent({
-        //   event_id: data.event_name,
-        //   image_link: data.image_link,
-        //   event_name: data.event_name,
-        //   event_type: data.event_type,
-        //   date: data.date,
-        //   location_address: data.location_address,
-        //   description: data.description,
-        // })
-        const metaTags = document.querySelectorAll('meta');
-        console.log(metaTags);
-        metaTags.forEach(tag => {
-          if (tag.getAttribute('property')?.includes('og:')) {
-            if (tag.getAttribute('property').includes('description')) {
-              ogTags.description = tag.content;
-            }
-            if (tag.getAttribute('property').includes('title')) {
-              ogTags.title = tag.content;
-            }
-            if (tag.getAttribute('property').includes('image')) {
-              ogTags.image = tag.content;
-            }
-          }
-        })
-        console.log(ogTags);         
-      })
+  useEffect(() => {
+    // request to backend with api prefix to get event data for populating frontend
+    fetch(`https://a90a-70-66-216-90.ngrok-free.app/api/e/${name}`, {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'ngrok-skip-browser-warning':'skip' // needed for local development only
+      },
+      mode: 'cors',
+    })
+    .then(res => {
+      // format fetch response
+      return res.json()})
+    .then(data => {      
+      // populate response into frontend state
+      setEvent({
+        event_id: data.event_id,
+        image_link: data.image_link,
+        event_name: data.event_name,
+        event_type: data.event_type,
+        date: data.date,
+        location_address: data.location_address,
+        description: data.description,
+      });      
+    })
     .catch(err => {
       console.error('Fetch error:', err);  
     })
+  },[name]);
 
   useEffect(() => {
-  }, [name])
+    // ensure event has been set then search and manipulate HTML
+    if (event.event_name) {
+      // find placeholder values and set dynamic values
+      document.title = event.event_name;
+      const metaTags = document.querySelectorAll('meta[content="___PLACEHOLDER___"]');
+      metaTags.forEach(tag => {        
+        if (tag.getAttribute('property')?.includes('og:')) {
+          if (tag.getAttribute('property').includes('description')) {
+            tag.content = event.description;
+          }
+          if (tag.getAttribute('property').includes('title')) {
+            tag.content = event.event_name;
+          }
+          if (tag.getAttribute('property').includes('image')) {
+            tag.content = event.image_link
+          }
+        }
+      });
+      // page is ready and conditional rendering state can be switched
+      setLoading(false);
+    };
+  },[event]);
+
     
   return (
     <>
-    {/* <Helmet>
-      <title>{event.title}</title>
-      <meta name="description" content={event.description} />
-      <meta property="og:image" content={event.image_link} />
-    </Helmet> */}
       <Text>Event Page</Text>
       {loading ? 
         <ActivityIndicator size='large' />
